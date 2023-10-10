a5ff7e63eaae39328b4a9d72aeafbb1e8cc3dc1a
Fedor Tarasov <tarassovf@gmail.com>
2023-10-10 09:33:50 +0300

    move callers to context

--- logic-engine/Runtime/commands/context/FunctionContext.js
+++ logic-engine/Runtime/commands/context/FunctionContext.js
@@ -2,6 +2,7 @@ export default class FunctionContext {
   constructor (contextData) {
     this.nodeResults = {}
     this.userData = contextData
+    this.callers = {}
   }
 
   getResult (uuid) {
@@ -10,5 +11,13 @@ export default class FunctionContext {
 
   setResult (uuid, data) {
     this.nodeResults[uuid] = data
+  }
+
+  setCaller (node, caller) {
+    this.callers[node.uuid] = caller.uuid
+  }
+
+  getCaller(node) {
+    return this.callers[node.uuid]
   }
 }
